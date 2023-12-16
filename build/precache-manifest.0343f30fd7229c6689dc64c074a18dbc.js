c6183781a3c1e7c87d8e601895545a18cd59cd42
Jarkko.O <cjaol004@edu.xamk.fi>
2023-12-16 10:43:05 +0200

    julkaisu

---
+++ build/precache-manifest.0343f30fd7229c6689dc64c074a18dbc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e94c3ab6a9fac9d18c216c881814fd24",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b75626740ca1b3788583",
+    "url": "/static/css/main.bf387121.chunk.css"
+  },
+  {
+    "revision": "d1072850c879b1076f45",
+    "url": "/static/js/2.8df519e5.chunk.js"
+  },
+  {
+    "revision": "5655a0cc71ffc732c2fb",
+    "url": "/static/js/3.e50d97f2.chunk.js"
+  },
+  {
+    "revision": "b75626740ca1b3788583",
+    "url": "/static/js/main.04392c1c.chunk.js"
+  },
+  {
+    "revision": "bc23ab808167254694f5",
+    "url": "/static/js/runtime~main.8cfe0669.js"
+  }
+]);
